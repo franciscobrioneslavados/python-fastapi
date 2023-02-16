2033b446509800750069dc9337ecf1f22f382674
Francisco Briones <franciscobrioneslavados@gmail.com>
2023-02-16 12:53:41 -0300

    clean repo

--- models/user_model.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-
-class UserModel(BaseModel):
-    id: Optional[str]
-    name: str
-    email: str
-    password: str
--- schemas/user_schema.py
+++
@@ -1,9 +0,0 @@
-def UserEntity(item) -> dict:
-    return {
-        "id": str(item["_id"]),
-        "name": item["name"],
-        "email": item["email"],
-        "password": item["password"]   
-    }
-def UsersEntity(items) -> dict:
-    return [UserEntity(item) for item in items]
